cae942a57e52b3358983e076befe5073c9353ad7
HAMZAMADAPURAM <hamzapadanna@gmail.com>
2026-02-13 09:47:57 +0530

    Initial commit - Node.js app with Docker

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end("Hello from Node.js DevOps App 🚀");
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
